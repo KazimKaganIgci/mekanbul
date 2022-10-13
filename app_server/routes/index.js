3925b9eba01416dc2db6f393c6d78b3056243f91
Kazım Kağan İğci <kazim-4@hotmail.com>
2022-10-13 21:05:48 +0300

    mekan eklendi

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var ctrlMain = require("../controllers/main");
+var ctrlDigerleri = require("../controllers/digerleri");
 
 /* GET home page. */
-router.get("/", ctrlMain.index);
-
+router.get("/", ctrlMain.anaSayfa);
+router.get("/mekan/yorum/yeni", ctrlMain.yorumEkle);
+router.get("/hakkinda", ctrlDigerleri.hakkinda);
+router.get("/mekan", ctrlMain.mekanBilgisi);
 module.exports = router;
